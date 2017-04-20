6de13e829e3fbeb8804717351f5a9c23f7dd03ce
Nevin Daniel <nevindnl@gmail.com>
2017-04-19 19:15:47 -0700

    update queuing methods

--- solution.py
+++ solution.py
@@ -3,41 +3,40 @@ def solution(filename):
     f = open(filename, 'r')
     text = f.readlines()
     n, k = map(lambda x: int(x), text[0].split())
-    prices = map(lambda x: int(x), text[1].split())
+    upvotes = map(lambda x: int(x), text[1].split())
     window_size = 1
-    subranges = deque([])
+    non_decreasing_subranges = deque([])
+    non_increasing_subranges = deque([])
     difference = 0
     for i in range(n):
         if i == 0:
             continue
-        enqueue(subranges, cmp(prices[i], prices[i - 1]))
-        difference += subranges[-1]
+        enqueue(non_decreasing_subranges, upvotes[i] >= upvotes[i - 1])
+        enqueue(non_increasing_subranges, upvotes[i] <= upvotes[i - 1])
+        difference += non_decreasing_subranges[-1] - non_increasing_subranges[-1]
+        print(non_decreasing_subranges, non_increasing_subranges, difference)
         window_size += 1
         if window_size == k:
             print(difference)
-            difference -= subranges[0]
-            dequeue(subranges)
+            difference -= non_decreasing_subranges[-1] - non_increasing_subranges[-1]
+            dequeue(non_decreasing_subranges)
+            dequeue(non_increasing_subranges)
+            print(non_decreasing_subranges, non_increasing_subranges, difference)
             window_size -= 1
 
 def dequeue(subranges):
-    first = subranges[0]
-    if first < -1:
-        subranges[0] += 1
-    elif first > 1:
+    if subranges[0] > 1:
         subranges[0] -= 1
     else:
         subranges.popleft()
 
 def enqueue(subranges, cmp):
-    if len(subranges) == 0:
-        subranges.append(cmp)
-        return
-    last = subranges[-1]
-    if last < 0 and cmp < 0:
-        subranges[-1] -= 1
-    elif last > 0 and cmp > 0:
-        subranges[-1] += 1
+    if cmp:
+        if len(subranges) == 0 or subranges[-1] == 0:
+            subranges.append(1)
+        else:
+            subranges[-1] += 1
     else:
-        subranges.append(cmp)
+        subranges.append(0)
 
 solution('input.txt')
